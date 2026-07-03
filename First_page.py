37487902b7c4c9a863b14127260333aacadf196e
akshs288 <aksht098@gmail.com>
2026-07-03 13:35:42 +0530

    Code before adding graphs

--- First_page.py
+++ First_page.py
@@ -107,24 +107,83 @@ def detail_bowl_player(name,file_path_o,file_path_t20,file_path_test):
     df_bowl_t20 = pd.read_csv(file_path_t20)
     df_bowl_test = pd.read_csv(file_path_test)
 
+    odi_name = df_bowl_o["Player"].str.split("(").str[0]
+    t20_name = df_bowl_t20["Player"].str.split("(").str[0]
+    test_name = df_bowl_test["Player"].str.split("(").str[0]
     
-    matches = list(df_bow.loc[df_bow["Player"].str.split("(").str[0] == name2,"Mat"])[0]
-    wickets = list(df_bow.loc[df_bow["Player"].str.split("(").str[0] == name2,"Wkts"])[0]
-    Avg = list(df_bow.loc[df_bow["Player"].str.split("(").str[0] == name2,"Ave"])[0]
-    Economoy = list(df_bow.loc[df_bow["Player"].str.split("(").str[0] == name2,"Econ"])[0]
-    tot_ball = list(df_bow.loc[df_bow["Player"].str.split("(").str[0] == name2,"Balls"])[0]
-    run = list(df_bow.loc[df_bow["Player"].str.split("(").str[0] == name2,"Runs"])[0]
+    tot_Wick = 0
+    tot_match = 0
+    tot_runs = 0
+    tot_balls = 0
+    
+    def avg_eco_bow(name_of_player):
+        Avg_ = 0
+        eco = 0
+        if name_of_player in odi_name.values:
+            Avg_ += float(list(df_bowl_o.loc[df_bowl_o["Player"].str.split("(").str[0] == name_of_player,"Ave"])[0])
+            eco += float(list(df_bowl_o.loc[df_bowl_o["Player"].str.split("(").str[0] == name_of_player,"Econ"])[0])
+            
+        elif name_of_player in t20_name.values:
+            Avg_ += float(list(df_bowl_t20.loc[df_bowl_t20["Player"].str.split("(").str[0] == name_of_player,"Ave"])[0])
+            eco += int(list(df_bowl_t20.loc[df_bowl_t20["Player"].str.split("(").str[0] == name_of_player,"Ave"])[0])
         
+        elif name_of_player in test_name.values:
+            Avg_ += float(list(df_bowl_test.loc[df_bowl_test["Player"].str.split("(").str[0] == name_of_player,"Ave"])[0])
+            eco += float(list(df_bowl_test.loc[df_bowl_test["Player"].str.split("(").str[0] == name_of_player,"Ave"])[0])
+        
+        return Avg_,eco
+
+    if name in odi_name.values:
+        wik_bowler_o = int(list(df_bowl_o.loc[df_bowl_o["Player"].str.split("(").str[0] == name,"Wkts"])[0])
+        tot_Wick += wik_bowler_o
+        
+        mat_o = int(list(df_bowl_o.loc[df_bowl_o["Player"].str.split("(").str[0] == name,"Mat"])[0])
+        tot_match += mat_o
+        
+        runs_o = int(list(df_bowl_o.loc[df_bowl_o["Player"].str.split("(").str[0] == name,"Runs"])[0])
+        tot_runs += runs_o
+                
+        bow_o = int(list(df_bowl_o.loc[df_bowl_o["Player"].str.split("(").str[0] == name,"Balls"])[0])
+        tot_balls += bow_o
+        
+    if name in t20_name.values:
+        wick_bowler_t20 = int(list(df_bowl_t20.loc[df_bowl_t20["Player"].str.split("(").str[0] == name,"Wkts"])[0])
+        tot_Wick += wick_bowler_t20
+    
+        mat_t20 = int(list(df_bowl_t20.loc[df_bowl_t20["Player"].str.split("(").str[0] == name,"Mat"])[0])
+        tot_match += mat_t20
+    
+        runs_t20 = int(list(df_bowl_t20.loc[df_bowl_t20["Player"].str.split("(").str[0] == name,"Runs"])[0])
+        tot_runs += runs_t20
+        
+        bow_t20 = int(list(df_bowl_t20.loc[df_bowl_t20["Player"].str.split("(").str[0] == name,"Overs"])[0])*6
+        tot_balls += bow_t20
+        
+    
+    if name in test_name.values:
+        wick_bowler_test = int(list(df_bowl_test.loc[df_bowl_test["Player"].str.split("(").str[0] == name,"Wkts"])[0])
+        tot_Wick += wick_bowler_test
+        
+        mat_test = int(list(df_bowl_test.loc[df_bowl_test["Player"].str.split("(").str[0] == name,"Mat"])[0])
+        tot_match += mat_test
+        
+        runs_test = int(list(df_bowl_test.loc[df_bowl_test["Player"].str.split("(").str[0] == name,"Runs"])[0])
+        tot_runs += runs_test
+        
+        bow_test = int(list(df_bowl_test.loc[df_bowl_test["Player"].str.split("(").str[0] == name,"Balls"])[0])
+        tot_balls += bow_test
+
     col9,col10,col11,col12 = st.columns(4)
     col13,col14,col15,col16 = st.columns(4)
-        
+    
+    Avg,Economy = avg_eco_bow(name)
     # # Now defining Stats
-    col9.metric("Total Wickets🎯",wickets,border = True)
-    col10.metric("Total Matches⚡",matches,border = True)
+    col9.metric("Total Wickets🎯",tot_Wick,border = True)
+    col10.metric("Total Matches⚡",tot_match,border = True)
     col11.metric("Average🔥",Avg,border = True)
-    col12.metric("Economy💣",Economoy,border = True)
-    col13.metric("Total Balls🥎",tot_ball,border = True)
-    col14.metric("Total Runs🏏",run,border = True)
+    col12.metric("Economy💣",Economy,border = True)
+    col13.metric("Total Balls🥎",tot_balls,border = True)
+    col14.metric("Total Runs🏏",tot_runs,border = True)
 
 if st.session_state["bat_or_bal"] == "Batsman":
     df_o = pd.read_csv("D:\\All Coding files\\DATA SETS\\Cricket\\odb.csv")
@@ -141,14 +200,11 @@ if st.session_state["bat_or_bal"] == "Batsman":
     
     detail_bat_player(name,"D:\\All Coding files\\DATA SETS\\Cricket\\odb.csv","D:\\All Coding files\\DATA SETS\\Cricket\\twb.csv","D:\\All Coding files\\DATA SETS\\Cricket\\tb.csv")
 
-
 elif st.session_state["bat_or_bal"] == "Bowler":
     df_bow = pd.read_csv("D:\\All Coding files\\DATA SETS\\Cricket\\odbo.csv")
     df4 = df_bow["Player"].str.split("(").str[0]
     dat2 = sorted(list(df4))
     name2 = st.selectbox("Bowling",dat2)
-    st.write("Key Bowling Stats")
-    
-    
-    
-    
+    st.write("Key Bowling Stats📊")
+    # file_path_o,file_path_t20,file_path_test
+    detail_bowl_player(name2,"D:\\All Coding files\\DATA SETS\\Cricket\\odbo.csv","D:\\All Coding files\\DATA SETS\\Cricket\\twbo.csv","D:\\All Coding files\\DATA SETS\\Cricket\\tbo.csv")
